5e7a1d62b6a36c04584d39ce025dfcc3c801f7ec
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-12-02 18:26:31 +0800

    Create utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+pub fn get_input(prompt: &str) -> String {
+    println!("{}", prompt);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
+}
